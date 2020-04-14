bf6a835d4f1c7f01440809767bae59a198571150
Szymon <szinton@gmail.com>
2020-04-14 15:02:35 +0200

    Testowy commit numer 2.

--- src/com/editor/Main.java
+++ src/com/editor/Main.java
@@ -1,4 +1,9 @@
 package com.editor;
 
+//wow! komentarz :O
+
 public class Main {
+    public static void main(String[] args) {
+        System.out.println("Prawie działający edytor tekstu.");
+    }
 }

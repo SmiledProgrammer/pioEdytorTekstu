0eb219b4c2e415a76dfb1f7ca0449f014dfa62d1
folejn <folejn@gmail.com>
2020-05-11 23:02:27 +0200

    Drobna zmiana klasy Language

--- src/com/editor/Language.java
+++ src/com/editor/Language.java
@@ -1,6 +1,14 @@
 package com.editor;
 
+import javax.swing.text.DefaultStyledDocument;
+import javax.swing.text.Style;
+import javax.swing.text.StyleContext;
+import javax.swing.text.StyledDocument;
+import java.awt.*;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 
 public class Language {
 
@@ -14,20 +22,26 @@ public class Language {
         }
     }
 
-    private ArrayList<String> keywords;
+    //private ArrayList<String> keywords; A może lepiej to zrobić za pomocą mapy:
+    private Map<String, Color> keywords;
     private ArrayList<SectionMarker> sectionMarkers;
 
     public Language() {
-        keywords = new ArrayList<String>();
+        //keywords = new ArrayList<String>();
+        keywords = new HashMap<>();
         sectionMarkers = new ArrayList<SectionMarker>();
     }
 
     public void updateTextColors() { //??? implementacja
+        /*String text = NotepadWindow.textPane.getText();
+        Style style = NotepadWindow.textPane.getStyle(StyleContext.DEFAULT_STYLE);
+        */
+
 
     }
 
-    public void addKeyword(String word) {
-        keywords.add(word);
+    public void addKeyword(String word, Color c) {
+        keywords.put(word, c);
     }
 
     public void addSectionMarker(char begin, char end) {

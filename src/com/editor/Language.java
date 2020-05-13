5a64c6dc323b0d2775875f84545816409de2930c
Szymon <szinton@gmail.com>
2020-05-13 16:58:43 +0200

    Dodałem przetwarzanie tekstu i kolorowanie tekstu do klasy Language.

--- src/com/editor/Language.java
+++ src/com/editor/Language.java
@@ -1,18 +1,17 @@
 package com.editor;
 
-import javax.swing.text.DefaultStyledDocument;
-import javax.swing.text.Style;
-import javax.swing.text.StyleContext;
-import javax.swing.text.StyledDocument;
+import javax.swing.*;
+import javax.swing.text.*;
 import java.awt.*;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
 
 
 public class Language {
 
-    private static class SectionMarker { //klasa przechowująca znaki rozpoczynające i kończące sekcję (np. "<" i ">", "{" i "}")
+    protected static class SectionMarker { //klasa przechowująca znaki rozpoczynające i kończące sekcję (np. "<" i ">", "{" i "}")
         public char beginning;
         public char ending;
 
@@ -22,22 +21,83 @@ public class Language {
         }
     }
 
-    //private ArrayList<String> keywords; A może lepiej to zrobić za pomocą mapy:
-    private Map<String, Color> keywords;
-    private ArrayList<SectionMarker> sectionMarkers;
+    protected Map<String, Color> keywords;
+    protected ArrayList<SectionMarker> sectionMarkers;
+    protected boolean highlightNumbers;
+
+    public static String word;
 
     public Language() {
-        //keywords = new ArrayList<String>();
         keywords = new HashMap<>();
         sectionMarkers = new ArrayList<SectionMarker>();
+        highlightNumbers = false;
     }
 
-    public void updateTextColors() { //??? implementacja
-        /*String text = NotepadWindow.textPane.getText();
-        Style style = NotepadWindow.textPane.getStyle(StyleContext.DEFAULT_STYLE);
-        */
+    protected static boolean isWhitespaceCharacter(char c) {
+        switch (c) {
+            case ' ': return true;
+            case '\n': return true;
+            case '\t': return true;
+            default: return false;
+        }
+    }
 
+    protected void changeTextColor(int pos, int length, Color color) { //funkcja zmieniająca kolor danego fragmentu tekstu (z tego co wiem trzeba usuwać tekst i wstawiać go na nowo z kolorkiem)
+        JTextPane pane = NotepadWindow.textPane;
+        StyledDocument doc = NotepadWindow.textPane.getStyledDocument();
+        Style style = pane.addStyle(color.toString(), null);
+        StyleConstants.setForeground(style, color);
+        try {
+            doc.remove(pos, length);
+            doc.insertString(pos, word, style);
+        } catch (BadLocationException ex) {}
+    }
 
+    protected int getNextWord(int startingIndex) { //zwraca indeks znalezionego słowa; słowo zapisuje w statycznej zmiennej globalnej "word"; startingIndex to indeks, od którego zaczynane jest sprawdzanie
+        word = "";
+        String text = NotepadWindow.textPane.getText();
+        int i = startingIndex;
+        int beginningIndex;
+        char c;
+        do { //szukanie początku słowa
+            c = text.charAt(i);
+            i++;
+        } while (isWhitespaceCharacter(c) && i < text.length());
+        beginningIndex = i - 1;
+        while (!isWhitespaceCharacter(c) && i < text.length()) { //szukanie końca słowa
+            word += c;
+            c = text.charAt(i);
+            i++;
+        }
+        if (!isWhitespaceCharacter(c) && i == text.length()) //dodawanie ostatniego znaku
+            word += c;
+        return beginningIndex;
+    }
+
+    protected void checkKeywords(int startingIndex) {
+        Iterator it = keywords.entrySet().iterator();
+        while (it.hasNext()) {
+            Map.Entry element = (Map.Entry) it.next();
+            String keyword = (String) element.getKey();
+            if (word.equals(keyword))
+                changeTextColor(startingIndex, word.length(), (Color) element.getValue());
+            it.remove();
+        }
+    }
+
+    public void updateTextColors() {
+        int index = 0;
+        word = "";
+        while (index + word.length() + 1 < NotepadWindow.textPane.getText().length()) {
+            index = getNextWord(index + word.length() + 1); // Pobieranie następnego słowa w tekście
+            checkKeywords(index); // Słowa kluczowe
+
+            // Liczby
+            /* do zrobienia (trzeba rozpoznawać liczby ze stringa, np. "15030", "1032.432", itp.) */
+
+            // Sekcje
+            /* do zrobienia */
+        }
     }
 
     public void addKeyword(String word, Color c) {
@@ -46,6 +106,16 @@ public class Language {
 
     public void addSectionMarker(char begin, char end) {
         sectionMarkers.add(new SectionMarker(begin, end));
+    }
+
+    public void setHighlightNumbers(boolean value) {
+        highlightNumbers = value;
+    }
+
+    //do usunięcia potem
+    public static void main(String[] args) {
+        Color c = new Color(100, 100, 50);
+        System.out.println(c.toString());
     }
 
 }

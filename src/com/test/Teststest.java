e2c318cea84ae56b317933a559e2284d24072e4c
BarKabal <bkabala00@gmail.com>
2020-05-23 15:52:27 +0200

    Dodałem Junit(?)

---
+++ src/com/test/Teststest.java
@@ -0,0 +1,14 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class Teststest {
+    @Test
+    public void firstTest() {
+        Assert.assertTrue(false);
+    }
+    @Test
+    public void secondTest() {
+        Assert.assertTrue(true);
+    }
+}
+
